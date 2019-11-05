fbe735bb3fc1c6698622dd4f2fbb95273df55e95
ElizabethTorre <etr604@gmail.com>
2019-11-05 18:51:23 -0500

    Filtros de categoria

--- src/controlador-firebase/controlador-fb.js
+++ src/controlador-firebase/controlador-fb.js
@@ -6,6 +6,15 @@ export const guardarPedidos = (arrObj) => {
    return firebase.firestore().collection('Pedidos').add(arrObj);
 }
 
+// export const readPosts = (callback) => {
+//    firebase.firestore().collection('catálogo').onSnapshot((datos) => {
+//      const array = [];
+//      datos.forEach((doc) => {
+//        array.push({ id: doc.id, ...doc.data() });
+//      });
+//      callback(array);
+//    });
+// };
 export const productos = (tipo, call) => {
    firebase.firestore().collection("catálogo").where("categoria", "==", tipo)
       // .get()
--- src/vistas/products.js
+++ src/vistas/products.js
@@ -1,8 +1,12 @@
 export default (obj) => {
     const viewProd = `
     <div>
-    <img  class ="card"src="${obj.url}">
-            
+      <img  class ="card"src="${obj.url}">
+      <p>${obj.producto}</p>
+      <p>${obj.marca}</p>
+      <p><span>Precio Venta </span>${obj.compra}</p>
+      <p><span>Precio Sugerido </span>${obj.precio}</p>
+      <button type="button">Agregar</button>
      </div> 
     `;
 
