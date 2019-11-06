edf16a9486839da9b4cbbeff27ab77983b4ba2c7
Betsycaceres <betsy0357@gmail.com>
2019-11-06 01:30:34 -0500

    creando array de productos seleccionados  localStorage

--- src/vistas/products.js
+++ src/vistas/products.js
@@ -10,24 +10,39 @@ export default (obj) => {
   <span id="cant"> 1 </span>
   <button id ="suma" type="button" class="btn-add">+</button>
   </div>
-  <button type="button" class="btn-add">Agregar</button>
+  <button  id="agregar"type="button" class="btn-add">Agregar</button>
 `;
-const divElement = document.createElement('section');
-divElement.className = "prod";
-divElement.innerHTML = viewProd;
-const cantNew = divElement.querySelector('#cant');
-const suma = divElement.querySelector('#suma');
-const resta = divElement.querySelector('#resta');
-suma.addEventListener('click', () => {
-  cantNew.innerHTML=parseInt(cantNew.textContent)+1;
-});
-resta.addEventListener('click', () => {
-  if(parseInt(cantNew.textContent)!==1) {
-  cantNew.innerHTML=parseInt(cantNew.textContent)-1;
-  } else {
-    cantNew=cantNew;
-  }
-});
+  const divElement = document.createElement('section');
+  divElement.className = "prod";
+  divElement.innerHTML = viewProd;
+  const cantNew = divElement.querySelector('#cant');
+  const suma = divElement.querySelector('#suma');
+  const resta = divElement.querySelector('#resta');
+  const agregar = divElement.querySelector('#agregar');
 
-return divElement;
+  suma.addEventListener('click', () => {
+    cantNew.innerHTML = parseInt(cantNew.textContent) + 1;
+  });
+  resta.addEventListener('click', () => {
+    if (parseInt(cantNew.textContent) !== 1) {
+      cantNew.innerHTML = parseInt(cantNew.textContent) - 1;
+    } else {
+      cantNew = cantNew;
+    }
+  });
+
+
+  agregar.addEventListener('click', () => {
+    const datosLocal = localStorage.getItem('datos')
+    const objeto = datosLocal == null ? { datos: [] } : JSON.parse(datosLocal)
+    const metodoFind = objeto.datos.find(ele => ele.id === obj.id);
+    if (!metodoFind) {
+      objeto.datos.push({ id: obj.id, cant: parseInt(cantNew.textContent), ...obj })
+    }else{
+      metodoFind.cant++
+    }
+    localStorage.setItem("datos", JSON.stringify(objeto))
+    console.log(localStorage.getItem('datos'))
+  })
+  return divElement;
 }
