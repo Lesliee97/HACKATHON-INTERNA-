a0d3c67b0c68ae79dfb69c64eb1917d3f758bbf5
Betsycaceres <betsy0357@gmail.com>
2019-11-06 00:51:55 -0500

    agregando usuario

--- src/vistas/index.js
+++ src/vistas/index.js
@@ -4,10 +4,12 @@ import loginOperario from './loginOperario.js';
 import Home from './home.js';
 import Different from './notFound.js';
 import Products from './products.js';
+import resumencompra from '../vistas/resumencompra.js';
 
 const components = {
     catalogo: catalogo,
     login:login,
+   resumencompra:resumencompra,
     loginOperario: loginOperario,
     home: Home,
     different: Different,
--- src/vistas/products.js
+++ src/vistas/products.js
@@ -1,21 +1,33 @@
 export default (obj) => {
-    const viewProd = `
-    
-      <img  class ="card"src="${obj.url}">
-      <p class="name-prod">${obj.producto}</p>
-      <p class="marca">${obj.marca}</p>
-      <p><span>Precio Venta </span>S/.${obj.compra}</p>
-      <p><span>Precio Sugerido </span>S/.${obj.precio}</p>
-      <div>
-      <button type="button" class="btn-add">-</button>
-      <span> </span>
-      <button type="button" class="btn-add">+</button>
-      </div>
-      <button type="button" class="btn-add">Agregar</button>
-   
-    `;
-    const divElement = document.createElement('section');
-    divElement.className = "prod";
-    divElement.innerHTML = viewProd;
-    return divElement;
+  const viewProd = `
+  <img  class ="card"src="${obj.url}">
+  <p class="name-prod">${obj.producto}</p>
+  <p class="marca">${obj.marca}</p>
+  <p><span>Precio Venta </span>S/.${obj.compra}</p>
+  <p><span>Precio Sugerido </span>S/.${obj.precio}</p>
+  <div>
+  <button id= "resta" type="button" class="btn-add">-</button>
+  <span id="cant"> 1 </span>
+  <button id ="suma" type="button" class="btn-add">+</button>
+  </div>
+  <button type="button" class="btn-add">Agregar</button>
+`;
+const divElement = document.createElement('section');
+divElement.className = "prod";
+divElement.innerHTML = viewProd;
+const cantNew = divElement.querySelector('#cant');
+const suma = divElement.querySelector('#suma');
+const resta = divElement.querySelector('#resta');
+suma.addEventListener('click', () => {
+  cantNew.innerHTML=parseInt(cantNew.textContent)+1;
+});
+resta.addEventListener('click', () => {
+  if(parseInt(cantNew.textContent)!==1) {
+  cantNew.innerHTML=parseInt(cantNew.textContent)-1;
+  } else {
+    cantNew=cantNew;
+  }
+});
+
+return divElement;
 }
